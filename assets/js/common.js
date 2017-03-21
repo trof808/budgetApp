09e920f640ab611f76b172a3ce86fcbf8ff1db87
trof <reatrof@gmail.com>
2017-03-22 00:38:41 +0300

    added option of saving expenses

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,43 @@
-/**
- * Created by trof on 21/03/2017.
- */
+$(document).ready(function() {
+
+    $('.nav-item').on('click', function() {
+        $('.nav-item').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('.pb-btn').on('click', function() {
+        $('.pb-btn').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('tr').mouseover(function() {
+        $(this).addClass('active');
+    });
+
+    $('tr').mouseleave(function() {
+        $(this).removeClass('active');
+    });
+
+    $('#pb-form-leave').on('submit', function() {
+
+        var typeLeave = 'расход';
+        var dateLeave = $('#pb-date').val();
+        var categoryLeave = $('#pb-tag').val();
+        var descriptionLeave = $('#pb-desc').val();
+        var sumLeave = $('#pb-sum').val();
+
+        var dataLeave = {type: typeLeave, date: dateLeave, category: categoryLeave, description: descriptionLeave, sum: sumLeave};
+
+        // console.log(sumLeave);
+
+        $.ajax({
+            type: 'POST',
+            url: '/',
+            data: dataLeave,
+            success: function(data) {
+                    location.reload();
+            }
+        });
+        return false;
+    });
+});
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,55 @@
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+mongoose.connect('mongodb://trof:585465077m@ds137230.mlab.com:37230/budgetwebapp');
+
+var pbLeave = new mongoose.Schema({
+    type: String,
+    date: String,
+    category: String,
+    description: String,
+    sum: Number
+});
+
+var LeaveDate = mongoose.model('LeaveDate', pbLeave);
+
+function formatDate(date) {
+    var day = date.getDate();
+    var monthIndex = date.getMonth() + 1;
+    var year = date.getFullYear();
+    monthIndex = monthIndex.toString();
+    if (monthIndex.length < 2) {
+        monthIndex = '0' + monthIndex;
+    }
+    var nowDate = year + '-' + monthIndex + '-' + day;
+    return nowDate;
+}
+
+var currentDate = formatDate(new Date());
+
 module.exports = function(app) {
     app.get('/', function(req, res) {
-        res.render('index');
+        var options = {
+            currenDate: currentDate,
+            data: {}
+        }
+
+        LeaveDate.find({}, function (err, data) {
+            if(err) throw err;
+            if(data !== {}) {
+                options.data = data
+            }
+            res.render('index', {options: options});
+        })
+
+    });
+
+    app.post('/', urlencodedParser, function(req, res) {
+        var newLeave = LeaveDate(req.body).save(function(err, data) {
+            if(err) console.log('Ошибка при сохранении' + err);
+            res.json(data);
+        });
     });
 }
